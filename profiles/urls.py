99fc40fa082293c29ff7c2b747e4323140a9c3a0
meena-rathi Maheshwari <meenakumari16@hotmail.com>
2024-07-02 16:40:57 +0000

    add views

---
+++ profiles/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from profiles import views
+
+urlpatterns = [
+    path('profiles/', views.ProfileList.as_view(), name='profile_list'),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view(), name='profile_detail'),
+]
